23e07dae0a9ec71e7522b2bd76326d442560a0ff
jishiting <312683897@qq.com>
2018-02-11 23:07:11 +0800

    add main function

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,4 +5,7 @@ public class App {
     public void method1(){
         System.out.print("s");
     }
+    public static void main(String [] arge){
+        System.out.println("this is the main test");
+    }
 }
